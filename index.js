a7d555b8c612aad6a0fba178cc1364c3fd2e237e
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-02-15 14:01:19 +0100

    yeah

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const app = express();
 
 // get info form api
 
-const url = "https://whois.fdnd.nl/api/v1/squad/squad-b-2022";
-const data = await fetch(url).then((response) => response.json());
-
-console.log(data);
+const url = "https://whois.fdnd.nl/api/v1/squad/";
 
 // Stel ejs in als template engine en geef de 'views' map door
 app.set("view engine", "ejs");
@@ -19,9 +16,17 @@ app.set("views", "./views");
 app.use(express.static("public"));
 
 // Maak een route voor de index
-app.get("/", function (req, res) {
-  // res.send('Hello World!')
-  res.render("index", data);
+
+app.get("/", (request, response) => {
+  console.log(request.query.squad);
+
+  let slug = request.query.squad || "squad-b-2022";
+  let orderBy = request.query.orderBy || "name";
+  let squadUrl = url + slug + "?orderBy=" + orderBy + "&direction=ASC";
+
+  fetchJson(squadUrl).then((data) => {
+    response.render("index", data);
+  });
 });
 
 // Stel het poortnummer in waar express op gaat luisteren
@@ -32,3 +37,14 @@ app.listen(app.get("port"), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get("port")}`);
 });
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error);
+}
