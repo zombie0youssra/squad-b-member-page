53060d519aaed500df28189f73c06005d235abc3
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-02-22 14:05:10 +0100

    push push

--- public/main.js
+++ public/main.js
@@ -1,13 +1,1 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      // entry.target.classList.remove("show");
-    }
-  });
-});
 
-const hiddenElements = document.querySelectorAll(".text");
-hiddenElements.forEach((el) => observer.observe(el));
