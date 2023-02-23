a02af8fe950dfa97c0e2530f406ba985a68a6289
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-02-23 12:41:05 +0100

    mouse is back

--- public/main.js
+++ public/main.js
@@ -1,8 +1,8 @@
-// let blob = document.querySelector(".blob");
+let blob = document.querySelector(".blob");
 
-// document.body.onpointermove = (Event) => {
-//   const { clientX, clientY } = Event;
+document.body.onpointermove = (Event) => {
+  const { clientX, clientY } = Event;
 
-//   blob.style.left = `${clientX}px`;
-//   blob.style.top = `${clientY}px`;
-// };
+  blob.style.left = `${clientX}px`;
+  blob.style.top = `${clientY}px`;
+};
