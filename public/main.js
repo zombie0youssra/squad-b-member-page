f1c1cd4165855bc56a6a67a2ed89328e96218463
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-02-23 11:41:21 +0100

    not xx but xy

--- public/main.js
+++ public/main.js
@@ -4,5 +4,5 @@ document.body.onpointermove = (Event) => {
   const { clientX, clientY } = Event;
 
   blob.style.left = `${clientX}px`;
-  blob.style.top = `${clientX}px`;
+  blob.style.top = `${clientY}px`;
 };
