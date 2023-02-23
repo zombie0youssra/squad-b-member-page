8a1e6a25db9fa42c23df91f7495bb572d995bcf0
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-02-23 11:18:23 +0100

    blurry mouse move styling

--- public/main.js
+++ public/main.js
@@ -1,7 +1,8 @@
 let blob = document.querySelector(".blob");
 
-document.body.onmousemove = (Event) => {
+document.body.onpointermove = (Event) => {
   const { clientX, clientY } = Event;
+
   blob.style.left = `${clientX}px`;
   blob.style.top = `${clientX}px`;
 };
